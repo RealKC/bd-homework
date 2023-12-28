0e8539de63b460f5ba3c6f7a248814488effcfb5
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-12-28 21:13:00 +0200

    server: Introduce sqlx to the project

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,15 @@
 use axum::{routing::get, Router};
+use sqlx::SqlitePool;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let pool = SqlitePool::connect("sqlite:data.sqlite")
+        .await
+        .expect("Failed to open database");
+
+    let app = Router::new().route("/", get(root)).with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
