383a003bef981f3082ec4c6ca69167f8aa9f2620
Mițca Dumitru <dumitru0mitca@gmail.com>
2024-01-03 16:53:31 +0200

    app/time: Add `date_from`

--- app/src/time.rs
+++ app/src/time.rs
@@ -4,6 +4,11 @@ pub fn now() -> glib::DateTime {
     glib::DateTime::now(&glib::TimeZone::local()).unwrap()
 }
 
+#[track_caller]
+pub fn date_from(unix: i64) -> glib::DateTime {
+    glib::DateTime::from_unix_local(unix).unwrap()
+}
+
 pub fn format_date(date: &glib::DateTime) -> GString {
     date.format("%d %B %Y").unwrap()
 }
